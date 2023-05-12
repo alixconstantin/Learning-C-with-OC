27691a42b9cfe2f70695e451ed816f8d04d62448
alixconstantin <64416579+alixconstantin@users.noreply.github.com>
2023-05-12 02:22:51 +0200

    first_program

---
+++ first_program/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 			     // Inclut des bibliothèques de fonctionnalités  ( directives de préprocesseur )
+#include <stdlib.h>
+
+int main()				 		 // C'est par la fonction main que le programme commence
+{
+	printf("Hello world \n");    // instructions
+	return 0;
+}
+
+//  Le système des commentaires est similaire a Javascript "//"  "/* */"
+// 	\n  retour à la ligne (= "Entrée") ;
+//  \t  tabulation
+

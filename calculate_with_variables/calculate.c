9fb854ad25c8dcc4c52fa236b0a3126d65f768be
alixconstantin <64416579+alixconstantin@users.noreply.github.com>
2023-05-29 06:30:34 +0200

    NorminetteCalculate.c

--- calculate_with_variables/calculate.c
+++ calculate_with_variables/calculate.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
 
-	/**
-	 * Pour obtenir un résultat décimal 
-	 * Il est nécessaire de préciser le .0
- 	*/
+/**
+* Pour obtenir un résultat décimal 
+* Il est nécessaire de préciser le .0
+*/
 
-	void	ft_division(void)
+void	ft_division(void)
 {
-	double	resultat = 0;
+	double	resultat;
+
+	resultat = 0;
 	resultat = 5.0 / 2.0;
 	printf ("5 / 2 = %lf \n", resultat);
 }
 
-
-	void	ft_summon(void)
+void	ft_summon(void)
 {
-	int resultat;
-	int nombreA;
-	int nombreB;
+	int	resultat;
+	int	nombre_a;
+	int	nombre_b;
 
 	resultat = 0;
-	nombreA = 0;
-	nombreB = 0;
+	nombre_a = 0;
+	nombre_b = 0;
 	printf("Entrez le nombre 1 : ");
-	scanf("%d", &nombreB);
+	scanf("%d", &nombre_a);
 	printf("Entrez le nombre 2 : ");
-	scanf("%d", &nombreA);
+	scanf("%d", &nombre_b);
 	resultat = nombreA + nombreB;
 	printf ("%d + %d = %d\n", nombreA, nombreB, resultat);
 }
 
-
-	int main ()
+int	main(void)
 {
 	ft_division();
 	ft_summon();
-	return(0);
+	return (0);
 }

66c2cbfd4b7b93bbcc1e06060fa05af25576a110
alixconstantin <64416579+alixconstantin@users.noreply.github.com>
2023-06-05 22:33:18 +0200

    pointerPart1

---
+++ create_init_pointers/prog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int	main(int argc, char *argv[])
+{
+	int number;
+	int *my_pointer;
+
+	my_pointer = &number;
+	number = 42;
+	printf("The value of variable number is %d\n", number);
+	printf("The adress of variable number is %p\n", &number);
+	printf("The value of the variable my_pointer is  %p\n", my_pointer);
+	printf("The value of the variable at the adress of my_pointer is %d\n", *my_pointer);
+	return (0);
+}

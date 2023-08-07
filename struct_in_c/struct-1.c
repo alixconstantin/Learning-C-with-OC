9d62c846db6b2261cfb8e242005182c2a77a3e39
alixconstantin <64416579+alixconstantin@users.noreply.github.com>
2023-08-07 13:25:35 +0200

    struct

---
+++ struct_in_c/struct-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int	main (void)
+{
+	struct NomDeVotreStructure
+	{
+		int variable1;
+		int variable2;
+		int autreVariable;
+		double nombreDecimal;
+	};
+	// Le ; a la fin de l'accolade après la fin d'une structure
+	// Est obligatoire
+	return	(0);
+}	
+
